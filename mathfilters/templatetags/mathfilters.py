8fab695bfd8e3f2fa66806f763d4fdd30cefcb14
Danilo Bargen <danilo@factor.ch>
2012-10-09 17:36:09 +0200

    Fixed type conversion bug

--- mathfilters/templatetags/mathfilters.py
+++ mathfilters/templatetags/mathfilters.py
@@ -3,11 +3,20 @@ from django.template import Library
 register = Library()
 
 
+def int_or_float(arg):
+    if isinstance(arg, int) or isinstance(arg, float):
+        return arg
+    try:
+        return int(arg)
+    except ValueError:
+        return float(arg)
+    
+
 @register.filter(is_safe=False)
 def sub(value, arg):
     """Subtracts the arg from the value."""
     try:
-        return int(value) - int(arg)
+        return int_or_float(value) - int_or_float(arg)
     except (ValueError, TypeError):
         try:
             return value - arg
@@ -19,7 +28,7 @@ def sub(value, arg):
 def mul(value, arg):
     """Multiplies the arg with the value."""
     try:
-        return int(value) * int(arg)
+        return int_or_float(value) * int_or_float(arg)
     except (ValueError, TypeError):
         try:
             return value * arg
@@ -31,7 +40,7 @@ def mul(value, arg):
 def div(value, arg):
     """Divides the arg by the value."""
     try:
-        return int(value) / int(arg)
+        return int_or_float(value) / int_or_float(arg)
     except (ValueError, TypeError):
         try:
             return value / arg
@@ -43,7 +52,7 @@ def div(value, arg):
 def absolute(value):
     """Returns the absolute value."""
     try:
-        return abs(int(value))
+        return abs(int_or_float(value))
     except (ValueError, TypeError):
         try:
             return abs(value)
